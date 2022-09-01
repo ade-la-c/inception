2e34004ea13762c8e8d56cd42f1b5f7c5826404b
ade-la-c <gringodlc@gmail.com>
2022-09-01 17:46:27 +0200

    nginx container done

--- srcs/requirements/mariadb/start.sh
+++ srcs/requirements/mariadb/start.sh
@@ -1,2 +1,2 @@
-echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '$ROOT_PW';" > /var/lib/mysql/init_file
+echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '$PASSWORD';" > /var/lib/mysql/init_file
 mysqld_safe --init-file=/var/lib/mysql/init_file
