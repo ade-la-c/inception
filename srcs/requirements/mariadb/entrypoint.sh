7fcdf0c83a861bd9322e01d9ff8917637c74641b
ade-la-c <gringodlc@gmail.com>
2022-09-07 11:15:31 +0200

    e

--- srcs/requirements/mariadb/entrypoint.sh
+++ srcs/requirements/mariadb/entrypoint.sh
@@ -1,17 +1,34 @@
-cat .setup 2> /dev/null
-if [ $? -ne 0 ]; then
-	# modification de la DB mysql
-	mysql_install_db
-	usr/bin/mysqld_safe --datadir=/var/lib/mysql &
+# cat .setup 2> /dev/null
+# if [ $? -ne 0 ]; then
+# 	# modification de la DB mysql
+# 	mysql_install_db
+# 	usr/bin/mysqld_safe --datadir=/var/lib/mysql &
 
-	# ping pour attendre que la DB soit accessible
-	while ! mysqladmin ping -h "$MARIADB_HOST" --silent; do
-	sleep 1
-	done
+# 	# ping pour attendre que la DB soit accessible
+# 	while ! mysqladmin ping -h "$MARIADB_HOST" --silent; do
+# 	sleep 1
+# 	done
 
-	#creation de la DB avec mariadb
-	eval "echo \"$(cat /tmp/create_db.sql)\"" | mariadb
-	touch .setup
+# 	#creation de la DB avec mariadb
+# 	eval "echo \"$(cat /tmp/create_db.sql)\"" | mariadb
+# 	touch .setup
+# fi
+# #lancement de la DB mysql
+# usr/bin/mysqld_safe --datadir=/var/lib/mysql
+
+
+if [ ! -d "/var/lib/mysql/wordpress" ]; then 
+
+    mysql_install_db
+    service mysql start
+	echo "DROP DATABASE wordpress;" | mysql -u root --password=root;
+	echo "CREATE USER 'root'@'%' IDENTIFIED BY 'root';" | mysql -u root --password=root;
+	echo "CREATE DATABASE wordpress;" | mysql -u root --skip-password;
+	echo "ALTER DATABASE wordpress CHARACTER SET = 'utf8mb4' COLLATE = 'utf8mb4_general_ci';" | mysql -u root --password=root;
+	echo "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY 'root' WITH GRANT OPTION;" |  mysql -u root --password=root;
+	echo "FLUSH PRIVILEGES;" |  mysql -u root --password=root;
+    service mysql stop 
 fi
-#lancement de la DB mysql
-usr/bin/mysqld_safe --datadir=/var/lib/mysql
+#sleep 5
+echo "ALTER USER 'root'@'localhost'IDENTIFIED BY '$DB_PASSWORD';" > /var/lib/mysql/pass-reset
+mysqld_safe --init-file=/var/lib/mysql/pass-reset
